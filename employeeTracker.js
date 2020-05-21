6bb6b237b596cb39251488907b2de5b942a213ba
jeffreyadamo <jeffreyadamo@gmail.com>
2020-05-21 16:53:09 -0700

    updated viewAllRoles to utilize mysql query to eliminate switch

--- employeeTracker.js
+++ employeeTracker.js
@@ -11,82 +11,95 @@ const cTable = require("console.table");
 // const updateEmployeeManager = () => require("./lib/updateEmployeeManager");
 // const viewAllRoles = () => require("./lib/viewAllRoles");
 
-
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "Adamologics#1",
-    database: "employeeTracker_db"
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "Adamologics#1",
+  database: "employeeTracker_db",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    start();
-})
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+});
 
-function start(){
-    console.log('');
-    inquirer.prompt([
-        //List navigation options
-        {
-            type: "list",
-            name: "nav",
-            message: "What would you like to do?",
-            choices: [
-                "View All Employees",
-                "View All Employees By Department",
-                "View All Employees By Manager",
-                "Add Employee",
-                "Remove Employee",
-                "Update Employee Role",
-                "Update Employee Manager",
-                "View All Roles",
-                "Quit"
-            ]
-        }
+function start() {
+  console.log("");
+  inquirer
+    .prompt([
+      //List navigation options
+      {
+        type: "list",
+        name: "nav",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Roles",
+          "View All Departments",
+          "View All Employees By Department",
+          // "View All Employees By Manager", BONUS
+          "Add Employee",
+          "Add Department",
+          "Add Role",
+          // "Remove Employee", BONUS
+          "Update Employee Role",
+          // "Update Employee Manager", BONUS
+          "Quit",
+        ],
+      },
     ])
-    .then(function(data){
-        console.log("User had chosen to: " + data.nav);
-        console.log("-------------------------------------");
-        switch (data.nav) {
-            case "View All Employees":
-              viewAllEmployees();
-              break;    
-            case "View All Employees By Department":
-              viewEmployeeDepartment();
-              break;
-            case "View All Employees By Manager":
-              viewEmployeeManager();
-              break;
-            case "Add Employee":
-              addEmployee();
-              break;
-            case "Remove Employee":
-              removeEmployee();
-              break;
-            case "Update Employee Role":
-              updateEmployeeRole();
-              break;
-            case "Update Employee Manager":
-              updateEmployeeManager();
-              break;
-            case "View All Roles":
-              viewAllRoles();
-              break;
-            case "Quit":
-              quit();
-              break;
-          }
+    .then(function (data) {
+      console.log("User had chosen to: " + data.nav);
+      console.log("-------------------------------------");
+      switch (data.nav) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+        case "View All Employees By Department":
+          viewEmployeeDepartment();
+          break;
+        // case "View All Employees By Manager":
+        //   viewEmployeeManager();
+        //   break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        // case "Remove Employee":
+        //   removeEmployee();
+        //   break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        // case "Update Employee Manager":
+        //   updateEmployeeManager();
+        //   break;
+
+        case "Quit":
+          quit();
+          break;
+      }
     })
-    .then(function(){
-      
-      start();
-    })
-    .catch(function(err) {
-        console.log(err);
-      });
+    // .then(function(){
+
+    //   start();
+    // })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
 // CRUD Functions
@@ -95,77 +108,205 @@ function start(){
 // CREATE
 // ========================================================
 
-function addEmployee(){
-    console.log("Create this function")
-    console.log("-------------------------------------");
-};
+function addEmployee() {
+  console.log("Create this function");
+  console.log("-------------------------------------");
+}
+
+var departments = ["Sales", "Engineering", "Finance", "Legal", "Management"];
+
+function addRole() {
+  console.log("Starting function addRole");
+  console.log("-------------------------------------");
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.log("results : ", res);
+
+    const myDeps = res.map(function (deps) {
+      return { name: deps.name, value: deps.id };
+    });
+    console.log(myDeps);
+
+    inquirer
+      .prompt([
+        //List navigation options
+        {
+          type: "input",
+          name: "title",
+          message: "What is the new role's title?",
+        },
+
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the new role's salary?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "What is the new role's department?",
+          choices: myDeps,
+        },
+      ])
+      .then(function (data) {
+        // console.log(
+        //   "User creates role: " +
+        //     data.title +
+        //     " with a salary of " +
+        //     data.salary +
+        //     " in the " +
+        //     data.department +
+        //     " department."
+        // );
+        // switch (data.department) {
+        //   case "Sales":
+        //     data.department = 1;
+        //     break;
+        //   case "Engineering":
+        //     data.department = 2;
+        //     break;
+        //   case "Finance":
+        //     data.department = 3;
+        //     break;
+        //   case "Legal":
+        //     data.department = 4;
+        //     break;
+        //   case "Management":
+        //     data.department = 5;
+        //     break;
+        // }
+        // console.log(
+        //   "User creates role: " +
+        //     data.title +
+        //     " with a salary of " +
+        //     data.salary +
+        //     " in the " +
+        //     data.department +
+        //     " department."
+        // );
+        connection.query(
+          "INSERT INTO role SET ?",
+          {
+            title: data.title,
+            salary: data.salary,
+            department_id: data.department,
+          },
+          function (err, res) {
+            if (err) throw err;
+            start();
+          }
+        );
+      });
+  });
+}
+
+function addDepartment() {
+  console.log("Starting funciton addDepartment");
+  console.log("-------------------------------------");
+  inquirer
+    .prompt([
+      //List navigation options
+      {
+        type: "input",
+        name: "department",
+        message: "What is the new department's name?",
+      },
+    ])
+    .then(function (data) {
+      console.log("User created the " + data.department + " department");
+      departments.push(data.department);
+      console.table(departments);
+      viewAllDepartments();
+      start();
+    });
+}
 
 // READ
 // ========================================================
 
-function viewAllEmployees(){
-  console.log("Create this function viewAllEmployees")
+function viewAllEmployees() {
+  console.log("Starting function viewAllEmployees");
   console.log("-------------------------------------");
-  connection.query(
-      "SELECT * FROM employee", function(err, res) {
-      if (err) throw err;
-      console.table('','','All Employees:', res);
-      start();
-    }
-    ).then(function(){
-      
-      console.log("wtf");
-    })
-};
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(
+      "",
+      "-------------------------------------",
+      "All Employees:",
+      "-------------------------------------",
+      res
+    );
+    start();
+  });
+}
 
-function viewAllRoles(){
-  console.log("Create this function")
+function viewAllRoles() {
+  console.log("Starting function viewAllRoles");
   console.log("-------------------------------------");
-};
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(
+      "",
+      "-------------------------------------",
+      "All Roles:",
+      "-------------------------------------",
+      res
+    );
+    start();
+  });
+}
 
-function viewEmployeeDepartment(){
-  console.log("Create this function")
+function viewAllDepartments() {
+  console.log("Starting function viewAllDepartments");
   console.log("-------------------------------------");
-};
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(
+      "",
+      "-------------------------------------",
+      "All Departments:",
+      "-------------------------------------",
+      res
+    );
+    start();
+  });
+}
 
-function viewEmployeeManager(){
-  console.log("Create this function")
+function viewEmployeeDepartment() {
+  console.log("Create this function");
   console.log("-------------------------------------");
-};
+}
+
+// function viewEmployeeManager(){
+//   console.log("Create this function")
+//   console.log("-------------------------------------");
+// };
 
 // UPDATE
 // ========================================================
 
-function updateEmployeeManager(){
-  console.log("Create this function")
-  console.log("-------------------------------------");
-};
+// function updateEmployeeManager(){
+//   console.log("Create this function")
+//   console.log("-------------------------------------");
+// };
 
-function updateEmployeeRole(){
-  console.log("Create this function")
+function updateEmployeeRole() {
+  console.log("Create this function");
   console.log("-------------------------------------");
-};
+}
 // DELETE
 // ========================================================
 
-function removeEmployee(){
-  console.log("Create this function")
+// function removeEmployee(){
+//   console.log("Create this function")
+//   console.log("-------------------------------------");
+// }
+
+function quit() {
+  console.log("Goodbye");
   console.log("-------------------------------------");
+  connection.end();
 }
-
-function quit(){
-      console.log("Goodbye")
-      console.log("-------------------------------------");
-      connection.end();
-  };
-
-
-
-
-
-
-
-
 
 // // From Homework 10: EmployeeSummary:
 // // ============================================================
